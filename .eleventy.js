addbc1d9b9d5445d84750c15a1fd93ec1e172bf9
Sarah Allen <sarah@ultrasaurus.com>
2021-04-14 17:29:51 -0400

    fix broken links to uploads directory -- simply copy those files

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,12 @@ module.exports = function (eleventyConfig) {
     // Alias `layout: post` to `layout: layouts/post.njk`
     eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
     eleventyConfig.addLayoutAlias("base", "layouts/base.liquid");
+    // Copy the `uploads/` directory
+    eleventyConfig.addPassthroughCopy("uploads");
+
 
     return {
+        passthroughFileCopy: true,
         dir: {
             input: "./src",   // source files
             output: "./_site" // static site generated here

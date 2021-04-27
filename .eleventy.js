a33a4ee0052ea7957e051a62993661e6f9e04317
Sarah Allen <sarah@ultrasaurus.com>
2021-04-26 20:22:29 -0400

    add scss via https://www.belter.io/eleventy-sass-workflow/

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function (eleventyConfig) {
+
+    // via https://www.belter.io/eleventy-sass-workflow/
+    // By default, anything listed in .gitignore or .eleventyignore will be 
+    // ignored by eleventy's watch process. If the compiled CSS is ignored, 
+    //eleventy --serve wouldn't rebuild of the html whenever sass is recompiled.
+    //workaround: opt-out of eleventy inspecting .gitignore with the following
+    eleventyConfig.setUseGitIgnore(false);
+
     // Alias `layout: post` to `layout: layouts/post.njk`
     eleventyConfig.addLayoutAlias("post", "layouts/post.liquid");
     eleventyConfig.addLayoutAlias("base", "layouts/base.liquid");
